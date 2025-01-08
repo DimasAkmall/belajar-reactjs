d1dceb31b38cf2682fcb11484c144276b2414cc6
Dimas Akmal <dimasawp13@gmail.com>
2025-01-08 08:03:32 +0700

    JS in JSX + Props

--- react_vite/src/hello-world/HelloWorld.jsx
+++ react_vite/src/hello-world/HelloWorld.jsx
@@ -1,10 +1,38 @@
 export default function HelloWorld() {
+    const props = {
+        text: "Ini text dari props",
+    }
     return (
         <>
-            <h1>Hello World</h1>
-            <p>i'm learning react now!</p>
+            {/* <HeaderHelloWorld text="Hello World" /> */}
+            <HeaderHelloWorld {...props} />
+            <ParagraphHelloWorld />
         </>
     )
 }
 
 // export default HelloWorld;
+
+function HeaderHelloWorld({ text = "Forget to give PROPS teks" }) {
+    return (
+        <h1
+            style={{
+                color: "black",
+                background: "salmon",
+            }}>
+            {text.toUpperCase()}
+        </h1>
+    )
+}
+
+function ParagraphHelloWorld() {
+    const text = "I'm Learning React Now!"
+    return (
+        <p
+            style={{
+                color: "red",
+            }}>
+            {text.toLowerCase()}
+        </p>
+    )
+}
