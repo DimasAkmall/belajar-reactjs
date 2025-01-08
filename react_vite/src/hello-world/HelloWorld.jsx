ed720db3116549de2d7e2a0325d626a7f7e92546
Dimas Akmal <dimasawp13@gmail.com>
2025-01-08 07:38:16 +0700

    Hello World Component + Update config multifile in vite.config.js

---
+++ react_vite/src/hello-world/HelloWorld.jsx
@@ -0,0 +1,10 @@
+export default function HelloWorld() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <p>i'm learning react now!</p>
+        </>
+    )
+}
+
+// export default HelloWorld;
--- react_vite/vite.config.js
+++ react_vite/vite.config.js
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import HelloWorld from "./src/hello-world/HelloWorld"
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+    plugins: [react()],
+    build: {
+        rollupOptions: {
+            input: {
+                index: "index.html",
+                hello_world: "hello-world.html",
+            },
+        },
+    },
 })
