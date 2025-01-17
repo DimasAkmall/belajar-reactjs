127671eaf95395d9f0ca8e7a848b2a62a4ced23c
Dimas Akmal <dimasawp13@gmail.com>
2025-01-17 07:31:56 +0700

    Using useMemo() + Fitur search Note

--- src/note/NoteList.jsx
+++ src/note/NoteList.jsx
@@ -1,21 +1,38 @@
 import Note from "./Note"
-import { useContext } from "react"
+import { useContext, useMemo, useRef, useState } from "react"
 import { NotesContext } from "./NoteContext"
 
 // export default function NoteList({ notes, onChange, onDelete }) { // Without Context Reducer
 export default function NoteList() {
     // Using Context Reducer
     const notes = useContext(NotesContext) // Using Context Reducer
+    const [search, setSearch] = useState("")
+    const searchInput = useRef(null)
+
+    const filteredNotes = useMemo(() => {
+        console.info("Filtering notes")
+        return notes.filter((note) => note.text.includes(search))
+    }, [notes, search])
+
+    function handleClick() {
+        console.info("Search", searchInput.current.value)
+        setSearch(searchInput.current.value)
+    }
+
     return (
-        <ul>
-            {notes.map((note) => (
-                <li key={note.id}>
-                    {/* // Without Context Reducer */}
-                    {/* <Note note={note} onChange={onChange} onDelete={onDelete} /> */}
-                    {/* // Using Context Reducer */}
-                    <Note note={note} />
-                </li>
-            ))}
-        </ul>
+        <div>
+            <input ref={searchInput} placeholder="Search" />
+            <button onClick={handleClick}>Search</button>
+            <ul>
+                {filteredNotes.map((note) => (
+                    <li key={note.id}>
+                        {/* // Without Context Reducer */}
+                        {/* <Note note={note} onChange={onChange} onDelete={onDelete} /> */}
+                        {/* // Using Context Reducer */}
+                        <Note note={note} />
+                    </li>
+                ))}
+            </ul>
+        </div>
     )
 }
